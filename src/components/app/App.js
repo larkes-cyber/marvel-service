239b64a7efb26afdd43b9ea4bb7740c668feac96
larkes-cyber <playegrovoy@gmail.com>
2022-02-26 20:29:46 +0300

    I created normal navigation

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -6,35 +6,22 @@ import { useState } from "react";
 import decoration from '../../resources/img/vision.png';
 import ErrorBoud from '../errorBouderlend/ErrorBouderlend';
 import ComicsList from "../comicsList/ComicsList";
-import { BrowserRouter as Router, Route, Switch} from "react-router-dom";
+import MainPage from "../pages/mainPage";
+import { BrowserRouter, Route, Routes} from "react-router-dom";
 const App = ()=> {
-    const [state,setState]=useState(null);
-    const onLoadIdChar=(id)=>{
-        setState(id);
-    }
+
     return (
-        <Router>
+        <BrowserRouter>
         <div className="app">
             <AppHeader/>
             <main>
-                <Switch>
-                    <Route exact path="/">
-                        <RandomChar/>
-                        <div className="char__content">
-                                <CharList onLoadIdChar={(id)=>onLoadIdChar(id)}/>
-                                    <ErrorBoud>
-                                     <CharInfo idChar={state}/>
-                                    </ErrorBoud>
-                        </div>
-                        <img className="bg-decoration" src={decoration} alt="vision"/>
-                    </Route>
-                    <Route exact path="/comics">
-                     <ComicsList/>
-                    </Route>
-                </Switch>
+                <Routes>
+                    <Route path="/" element={<MainPage/>}/>
+                    <Route path="/comics" element={<ComicsList/>}/>
+                </Routes>
             </main>
         </div>
-    </Router>        
+        </BrowserRouter>        
       ) 
     
 }
--- src/components/appHeader/AppHeader.js
+++ src/components/appHeader/AppHeader.js
@@ -1,18 +1,18 @@
 import './appHeader.scss';
-
+import { Link,NavLink } from 'react-router-dom';
 const AppHeader = () => {
     return (
         <header className="app__header">
             <h1 className="app__title">
-                <a href="#">
+                <Link to="/">
                     <span>Marvel</span> information portal
-                </a>
+                </Link>
             </h1>
             <nav className="app__menu">
                 <ul>
-                    <li><a href="#">Characters</a></li>
+                    <li><NavLink to="/" style={({ isActive }) => ({ color: isActive ? '#9f0013' : 'black' })}>Characters</NavLink></li>
                     /
-                    <li><a href="#">Comics</a></li>
+                    <li><NavLink to="comics" style={({ isActive }) => ({ color: isActive ? '#9f0013' : 'black' })}>Comics</NavLink></li>
                 </ul>
             </nav>
         </header>
