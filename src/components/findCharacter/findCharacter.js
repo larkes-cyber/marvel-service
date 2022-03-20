b3b43da8ae2d09edcbfcd12ac62cab212d296c13
larkes-cyber <playegrovoy@gmail.com>
2022-03-20 11:09:32 +0300

    use formik

--- src/components/findCharacter/findCharacter.js
+++ src/components/findCharacter/findCharacter.js
@@ -1,12 +1,25 @@
 import './findCharacter.css';
+import { Formik, Form, Field, ErrorMessage } from 'formik';
+import * as yup from 'yup';
 
 const FindCharacter = () => {
     return (
-        <form className='form'>
-            <label htmlFor="name" id='labelName'>Or find a character by name:</label>
-            <input type="name" id='name' value="Enter name"/>
-            <button className='button button__main sumbit' type='sumbit'><div className="inner">Find</div></button>
-        </form>
+        <Formik
+            initialValues={{
+                name:"Enter name"
+            }}
+            validationSchema = {yup.object({
+                    name:yup.string()
+                            .required("Validation error")       
+                })
+            }
+        >
+            <form className='form'>
+                <label htmlFor="name" id='labelName'>Or find a character by name:</label>
+                <Field type="text" id='name' name="name"/>
+                <button className='button button__main sumbit' type='sumbit'><div className="inner">Find</div></button>
+            </form>         
+        </Formik>
     )
 }
 
